872c2053f187b3680812b13942e7b22a94fce50f
Awais saeed <mawais2017@namal.edu.pk>
2018-11-07 02:01:35 +0000

    Task_1 last function added

--- Task_1.cpp
+++ Task_1.cpp
@@ -5,12 +5,12 @@ class MyVector{
 private:
   double *A;
   int length;
+  int index;
 public:
-  static int index;
   friend ostream& operator <<(ostream& out,MyVector v);
-   MyVector(){}
-   ~MyVector(){}
-   MyVector(int l):length(l){
+   MyVector():index(0){}
+   ~MyVector(){free(A);}
+   MyVector(int l):index(0),length(l){
      A=(double*)malloc (length*sizeof(double));
    }
    MyVector(MyVector &temp_vector)
@@ -31,14 +31,14 @@ public:
      else
      return true;
    }
-   void operator = (MyVector &v)
+   void operator = (const MyVector &v)
    {
      length=v.length;
-     A=new double[length];
+     //A=new double[length];
      for(int i=0;i<length;i++)
      A[i]=v.A[i];
    }
-   double operator -(MyVector v)
+   double operator -(MyVector v)  // '-' Use for inner product operator
    {
      double product=0;
      if(length==v.length)
@@ -50,20 +50,28 @@ public:
      else
      cout<<"Error"<<endl;
    }
-   void operator *(int k)
+   MyVector operator *(int k)
    {
+     MyVector temp;
+     temp.A=new double[length];
     for(int i=0;i<length;i++)
-    A[i]=k*A[i];
+    temp.A[i]=(this->A[i])*k;
+    return temp;
    }
-   void operator +(MyVector v)
+   MyVector operator +(MyVector v)
    {
-     /*MyVector temp_v;
+     MyVector temp_v;
      temp_v.length=length;
      temp_v.A=new double[length];
-    */
-    for(int i=0;i<length;i++)
-    A[i]=A[i]+v.A[i];
-    //return temp_v;
+     for(int i=0;i<length;i++)
+     temp_v.A[i]=this->A[i]+v.A[i];
+     return temp_v;
+   }
+
+   void setlength(int l)
+   {
+     length=l;
+     A=(double*)malloc (length*sizeof(double));
    }
 
    int size_v()
@@ -76,7 +84,10 @@ public:
    }
    void del()
   {
+    if(index>=0)
      A[--index]=0;
+     else
+     cout<<"\nError";
    }
    void show_V()
    {
@@ -89,28 +100,34 @@ public:
 };
 ostream& operator <<(ostream& out,MyVector v)
 {
-  out<<"vector: ";
   for(int i=0;i<v.length;i++)
   out<<v.A[i]<<" ";
   out<<endl;
   return out;
 }
-int MyVector::index=0;
 int main(int argc, char const *argv[]) {
-  MyVector v1(3),v2,v3;
+
+  MyVector v1(4),v2(5),v3,v4;
   v1.add(1);
   v1.add(2);
   v1.add(3);
-  v2=v1;
-  /*v1.show_V();
+  v1.add(4);
+
+  v2.add(1);
+  v2.add(2);
+  v2.add(3);
+  v2.add(4);
+  v2.add(5);
+  v3.setlength(5);
+  v4.setlength(5);
+  v3=v1+v2;
+  v4=v2*3;
+  cout<<"V1: "<<v1;
+  cout<<"V2: "<<v2;
+  cout<<"V3=v1+v2: "<<v3;
+  cout<<"V4=v2*3: "<<v4;
+  v2.del();
   v2.show_V();
-  v1.del();
-  v1.show_V();
-  v2.show_V();
-*/
-v1+v2;
-//cout<<"Element:"<<v1[0]<<"Size "<<v1.size_v();
-//(v1*4);
-cout<<"const multiply: "<<v1;
+
   return 0;
 }
